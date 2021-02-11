be77cc629b35ee52c9df551b38aac1d8910b7a96
Jiyang Tang <jt304@duke.edu>
2021-02-11 17:46:54 +0800

    Use emoji unicode to print out cell state in Fire

--- src/Model/StateFire.java
+++ src/Model/StateFire.java
@@ -10,9 +10,9 @@ public enum StateFire implements State {
   @Override
   public String toString() {
     return switch (this) {
-      case EMPTY -> "E";
-      case TREE -> "T";
-      case BURNING -> "B";
+      case EMPTY -> "\u3000"; // space with the same width as emoji
+      case TREE -> "\uD83C\uDF32";
+      case BURNING -> "\uD83D\uDD25";
     };
   }
 }
