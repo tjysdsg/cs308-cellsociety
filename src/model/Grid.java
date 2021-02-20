865d61c21de3d6cb0ea2d6605b0cbda61ffddc33
Jiyang Tang <jt304@duke.edu>
2021-02-20 20:49:13 +0800

    Implement hex and triangle grid

--- src/model/Grid.java
+++ src/model/Grid.java
@@ -50,9 +50,25 @@ public abstract class Grid {
   }
 
   public void update() {
+    for (int r = 0; r < getNumRows(); ++r) {
+      for (int c = 0; c < getNumCols(); ++c) {
+        grid.get(r).get(c).update();
+      }
+    }
   }
 
-  public abstract List<Cell> getNeighborsOf(int r, int c);
+  public List<Cell> getNeighborsOf(int r, int c) {
+    ArrayList<Cell> ret = new ArrayList<>();
+    Vec2D coord = new Vec2D(r, c);
+
+    for (Vec2D delta : Neighborhood.ALL_NEIGHBOR_DIRECTIONS) {
+      Vec2D newCoord = coord.add(delta);
+      if (isInside(newCoord.getX(), newCoord.getY()) && neighborhood.isValidNeighbor(delta)) {
+        ret.add(getCell(newCoord.getX(), newCoord.getY()));
+      }
+    }
+    return ret;
+  }
 
   @Override
   public String toString() {
--- src/model/GridSq.java
+++ src/model/GridSq.java
@@ -1,8 +1,5 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Grid that has cells with square shape
  * <p>
@@ -13,27 +10,5 @@ public class GridSq extends Grid {
 
   public GridSq(int nRows, int nCols, State defaultState, Neighborhood neighborhood) {
     super(nRows, nCols, defaultState, neighborhood);
-  }
-
-  @Override
-  public void update() {
-    for (int r = 0; r < getNumRows(); ++r) {
-      for (int c = 0; c < getNumCols(); ++c) {
-        grid.get(r).get(c).update();
-      }
-    }
-  }
-
-  public List<Cell> getNeighborsOf(int r, int c) {
-    ArrayList<Cell> ret = new ArrayList<>();
-    Vec2D coord = new Vec2D(r, c);
-
-    for (Vec2D delta : Neighborhood.ALL_NEIGHBOR_DIRECTIONS) {
-      Vec2D newCoord = coord.add(delta);
-      if (isInside(newCoord.getX(), newCoord.getY()) && neighborhood.isValidNeighbor(delta)) {
-        ret.add(getCell(newCoord.getX(), newCoord.getY()));
-      }
-    }
-    return ret;
   }
 }
