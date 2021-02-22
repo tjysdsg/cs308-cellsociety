77956bc215abf1ba2b6a1015f9508fb48d04ba7c
Tinglong Zhu <tz88@duke.edu>
2021-02-22 08:34:40 +0800

    adding two simu parser

---
+++ src/controller/xml/xmlparser/LangtonXMLParser.java
@@ -0,0 +1,47 @@
+package controller.xml.xmlparser;
+
+import controller.xml.XMLException;
+import model.Simulation;
+import model.SimulationLangton;
+import model.SimulationPercolation;
+import model.State;
+import model.StateEnumLangton;
+import model.StateEnumPercolation;
+
+public class LangtonXMLParser extends XMLParser{
+
+  /**
+   * Create parser for XML files of given filename.
+   *
+   * @param fileName
+   */
+  public LangtonXMLParser(String fileName) throws XMLException {
+    super(fileName);
+  }
+
+  @Override
+  public Simulation getSimulation() throws XMLException {
+    root = getRootElement();
+    sizeX = getGridSizeX();
+    sizeY=getGridSizeY();
+    simulation = new SimulationLangton(sizeX,sizeY);
+    initSimulation();
+    return simulation;
+
+  }
+
+  @Override
+  public void initSimulation() throws XMLException {
+    simulation= new SimulationLangton(sizeX,sizeY);
+    super.initSimulation();
+  }
+
+  @Override
+  public void initStateArray() {
+    stateRange = StateEnumLangton.ALL_VALS.length;
+    states = new State[stateRange];
+    for (int val : StateEnumLangton.ALL_VALS) {
+      states[val] = new State(StateEnumLangton.fromInt(val));
+    }
+  }
+}
